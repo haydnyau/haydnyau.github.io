53da857a0552cae25a350b7ded526f7a515038dc
qiuhaidong <690982322@qq.com>
2022-03-26 03:10:10 +0800

    Site updated at: 2022-03-25 19:10:10 UTC

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 mv /home/dong/.ssh3 /home/dong/.ssh
+mv /home/dong/octopress/source/_posts/ /home/dong/haydnyau/
 jekyll build
 octopress deploy
 mv /home/dong/.ssh /home/dong/.ssh3
+mv /home/dong/haydnyau/_posts/ /home/dong/octopress/source/
